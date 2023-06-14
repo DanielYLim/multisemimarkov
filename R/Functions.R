5072ceb6d31011a3a48a3edf4c9ccfa379e0bbb3
Daniel Lim <64420518+DanielYLim@users.noreply.github.com>
2023-06-13 21:38:42 -0400

    Create r.yml
    1. Log logistic function added in details hazardLL.md
    2. some minor changes

--- R/Functions.R
+++ R/Functions.R
@@ -8,6 +8,11 @@
 #' @return hazard function of t with parameters alpha1 adn beta1.
 #' @export
 #'
+#' @details
+#' Additional details...
+#'  \deqn{h(t|alpha1, beta1) = ((beta1/alpha1)*(t/alpha1)^{beta1-1})/(1+(t/alpha1)^{beta1})}
+#'
+#'
 #' @examples
 #' hazardLL(1,2,1.3)
 #' \dontrun{
