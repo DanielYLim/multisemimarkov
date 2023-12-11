323d8574fde2a4d9b5f1dd8c157cbe9809be5a58
DanielYLim <daniel.yongho.lim@gmail.com>
2023-12-11 12:53:18 -0500

    Commit #9 : second stage method estimation function and simulation function added

--- R/Simulations.R
+++ R/Simulations.R
@@ -78,6 +78,107 @@ return(Data_out)
 
 
 
+
+#' Estimation unmasked data with two stage method
+#'
+#' @param n number of observation
+#' @param alpha12 log-logstic scale parameter; 1->2 transition.
+#' @param beta12 log-logstic shape parameter; 1->2 transition.
+#' @param alpha13 log-logstic scale parameter; 1->3 transition.
+#' @param beta13 log-logstic shape parameter; 1->3 transition.
+#' @param alpha23 Weibull scale parameter; 2->3 transition.
+#' @param beta23 Weibull shape parameter; 2->3 transition.
+#' @param tau Kendall's tau; dependency measure
+#' @param p Probability of not-being cured.
+#' @param C_max Maximum value of censoring time.
+#' @param alpha04 Weibull scale parameter; cured -> 4 transition.
+#' @param beta04 Weibull shape parameter; cured -> 4 transition.
+#' @return nlm output with some information of generated data
+#' @export
+#' @examples
+#' B=10
+#' n=200
+#' alpha12 = 2
+#' beta12 = 4
+#' alpha13 = 3.5
+#' beta13 = 3
+#' alpha23 = 2.5
+#' beta23 = 1.5
+#' tau = 0.3
+#' p = 0.7
+#' C_max = 10
+#' alpha04 = 7
+#' beta04 = 4
+#' Result <- Estimation_unmasked_two_stage(n, alpha12, beta12, alpha13, beta13, alpha23, beta23, tau, p, C_max, alpha04, beta04)
+#' print(Result$estimate)
+Estimation_unmasked_two_stage<-function(n=200, alpha12 = 2, beta12 = 4, alpha13 = 3.5, beta13 = 3, alpha23 = 2.5, beta23 = 1.5, tau = 0.3, p = 0.7, C_max = 10, alpha04 = 7, beta04 = 4){
+
+
+  Data <- data_generation_ver1(n=n, alpha12 = alpha12, beta12 = beta12, alpha13 = alpha13, beta13 = beta13, alpha23 = alpha23, beta23 = beta23, tau = tau, p = p, C_max = C_max, alpha04 = alpha04, beta04 = beta04)
+
+
+  phi <- 2/(1-tau)-2 # parameter of the Clayton copula
+  initial_parms <- c(alpha12, beta12, alpha13, beta13, alpha23, beta23, phi, p)
+
+  t1 <- Data$t1
+  t2 <- Data$t2
+  status12 = Data$status12
+  status13 = Data$status13
+  status23 = Data$status23
+  n  = length(t1)
+
+  ind = status12==1
+  sum(ind)
+  Cenrate_12 <- 1-sum(ind)/n
+
+  ind = status13==1
+  sum(ind)
+  Cenrate_13 <- 1-sum(ind)/n
+
+  ind = status23==1
+  sum(ind)
+  Cenrate_23 <- 1-sum(ind)/n
+
+
+  nLL <- make.NegLogLik_first_stage(Data)
+  initial_parms <- c(alpha12, beta12, alpha13, beta13, p)
+  estimates <- nlm(nLL, initial_parms)$estimate
+
+  alpha12hat = estimates[1]
+  beta12hat = estimates[2]
+  alpha13hat = estimates[3]
+  beta13hat = estimates[4]
+
+
+  F101<-sapply(t1, f10j, alphajk=alpha12hat, betajk=beta12hat, alphajl=alpha13hat, betajl=beta13hat)
+
+  F101.star.hat.new=F101
+
+  pi1<- cubature::adaptIntegrate(f10j, lowerLimit=0, upperLimit=20, alphajk=alpha12hat, betajk=beta12hat, alphajl=alpha13hat, betajl=beta13hat)$integral
+
+  F101.star.hat.new = F101 / pi1
+
+  status12 <- Data$status12
+  status23 <- Data$status12
+
+
+  initial_parms <- c(0.7,1.3,0.8)
+  estimates_2 <- nlm(log_lik_second_stage,initial_parms)$estimate
+
+  Data_out <- list()
+  Data_out$estimates <- c(estimates[1:4], estimates_2, estimates[5])
+  Data_out$n <- n
+  Data_out$Cenrate_12 <- Cenrate_12
+  Data_out$Cenrate_13 <- Cenrate_13
+  Data_out$Cenrate_23 <- Cenrate_23
+
+  return(Data_out)
+}
+
+
+
+
+
 #' Simulation unmasked data
 #'
 #' @param B number of simulation runs
@@ -129,6 +230,57 @@ Simulation_unmasked<-function(B=10, n=200, alpha12 = 2, beta12 = 4, alpha13 = 3.
 }
 
 
+
+
+#' Simulation unmasked data using two stage method
+#'
+#' @param B number of simulation runs
+#' @param n number of observations.
+#' @param alpha12 log-logstic scale parameter; 1->2 transition.
+#' @param beta12 log-logstic shape parameter; 1->2 transition.
+#' @param alpha13 log-logstic scale parameter; 1->3 transition.
+#' @param beta13 log-logstic shape parameter; 1->3 transition.
+#' @param alpha23 Weibull scale parameter; 2->3 transition.
+#' @param beta23 Weibull shape parameter; 2->3 transition.
+#' @param tau Kendall's tau; dependency measure
+#' @param p Probability of not-being cured.
+#' @param C_max Maximum value of censoring time.
+#' @param alpha04 Weibull scale parameter; cured -> 4 transition.
+#' @param beta04 Weibull shape parameter; cured -> 4 transition.
+#' @return list of estimation outputs from 1:B
+#' @export
+#' @examples
+#' B=5
+#' n=200
+#' alpha12 = 2
+#' beta12 = 4
+#' alpha13 = 3.5
+#' beta13 = 3
+#' alpha23 = 2.5
+#' beta23 = 1.5
+#' tau = 0.3
+#' p = 0.7
+#' C_max = 10
+#' alpha04 = 7
+#' beta04 = 4
+#' Sim_result <- Simulation_unmasked_two_stage(B, n, alpha12, beta12, alpha13, beta13, alpha23, beta23, tau, p, C_max, alpha04, beta04)
+#' total_num <- length(Sim_result)
+#'estimations <- c()
+#'for (i in 1:total_num){
+#'  estimation_temp <- Sim_result[[i]]$estimate
+#'  estimations <- rbind(estimations, estimation_temp)
+#'}
+#'print(estimations)
+Simulation_unmasked_two_stage<-function(B=5, n=200, alpha12 = 2, beta12 = 4, alpha13 = 3.5, beta13 = 3, alpha23 = 2.5, beta23 = 1.5, tau = 0.3, p = 0.7, C_max = 10, alpha04 = 7, beta04 = 4){
+
+  Data_out <-list()
+  for (i in 1:B){
+    Data_out_temp <- Estimation_unmasked_two_stage(n, alpha12, beta12, alpha13, beta13, alpha23, beta23, tau, p, C_max, alpha04, beta04)
+    Data_out[[i]] <- Data_out_temp
+  }
+
+  return(Data_out)
+}
 
 
 
